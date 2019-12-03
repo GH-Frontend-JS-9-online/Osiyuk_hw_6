f2b40fb6b28ae595536bbf2f6d2c0e2160418ba3
Maxim Osiyuk <dalannok@gmail.com>
2019-12-04 00:56:33 +0200

    Homewokr 6 (50%)

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,15 @@
+
+// Create function which will return function with callback in arguments
+function callbackFunc(callback) {
+   if(callback && typeof callback ==='function');
+   callback();
+}
+
+function consoleText() {
+  console.log('Some text to be sure that callback is working');
+}
+
+
 //* Create function with 2 string params. It must check if letters in the first
 //* correspond to the number of matches in second and return %.
 function checkText(a, b) {
